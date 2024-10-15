ef1d9574ec4402aaa30ef2b09bd8c9d551ee3b34
Arstin Reano <arstingabrelreano@gmail.com>
2024-10-15 20:07:17 +0800

    Updating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,16 +8,35 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = article.new
+    @article = Article.new
   end
 
   def create
-    @article = article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+      
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
